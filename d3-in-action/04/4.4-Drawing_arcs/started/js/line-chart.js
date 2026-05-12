7bbe43bc5097520a781ef04bb17bbdd53b76c0bd
makr <cto@vizualizer.com>
2026-05-12 14:08:06 +0800

    - line chart explanation completed - js code for arc chart completed

--- d3-in-action/04/4.4-Drawing_arcs/started/js/line-chart.js
+++ d3-in-action/04/4.4-Drawing_arcs/started/js/line-chart.js
@@ -8,20 +8,27 @@ function showElapsedTime(stepName) {
   tik = performance.now();
 }
 
+const aubergine = "red";
+const paperBG = "#e7e2cd";
+
 // Load the data here
 Promise.all([
   d3.csv("data/weekly_temperature.csv", d3.autoType),
+  d3.csv("data/daily_precipitations.csv", d3.autoType),
   d3.json("/d3-in-action/asset/ko-KR.json"),
 ])
-  .then(([data, koKR]) => {
-    if (data && koKR) {
+  .then(([data, data2, koKR]) => {
+    if (data && data2 && koKR) {
       console.log("> temp data loaded ", data);
+      // 강우 데이터를 모두 불러오면 아래처럼 데이터를 출력한다.
+      console.log("> precipitations data loaded ", data2);
       console.log("> locale data ", koKR);
 
       // ---------- loading completed
       showElapsedTime("loading");
 
       drawLineChart(data);
+      drawArcChart(data2);
     } else {
       console.log("Resource found but the error with the response.");
       console.error("Loading failed");
@@ -32,7 +39,9 @@ Promise.all([
     console.error();
   });
 
-// Create the line chart here
+// --------------------------------------
+// Plot the line chart with the given data
+// --------------------------------------
 const drawLineChart = (data) => {
   /*******************************/
   /*    Declare the constants    */
@@ -74,7 +83,7 @@ const drawLineChart = (data) => {
     .attr("height", height - strokeWidth)
     .attr("x", strokeWidth / 2)
     .attr("y", strokeWidth / 2)
-    .attr("fill", "white")
+    .attr("fill", paperBG)
     .attr("stroke", "black")
     .attr("stroke-width", strokeWidth);
 
@@ -88,7 +97,7 @@ const drawLineChart = (data) => {
     .attr("class", "innerChartBG")
     .attr("width", `${innerWidth}`)
     .attr("height", `${innerHeight}`)
-    .attr("fill", "#e7e2cd");
+    .attr("fill", paperBG);
 
   /****************************/
   /*    Declare the scales    */
@@ -118,7 +127,6 @@ const drawLineChart = (data) => {
   /***************************/
 
   // const aubergine = "#75485E";
-  const aubergine = "red";
 
   /***************************/
   /*     ADD AREA
@@ -207,7 +215,7 @@ const drawLineChart = (data) => {
   /***************************/
   /*     Append the labels   */
   /***************************/
-  // 간단하게 코드양을 줄이려면, 
+  // 간단하게 코드양을 줄이려면,
   // 특정 날짜와 레이블의 위치를 입력받아
   // 날짜에 해당하는 점에서 위 혹은 아래로 선을 그리고
   // 거기에 레이블을 추가하는 함수를 만들 수도 있다.
@@ -218,9 +226,9 @@ const drawLineChart = (data) => {
     .attr("class", "label-max-temp")
     .attr("x", xScale(data[data.length - 4].date) + 15)
     .attr("y", yScale(data[data.length - 4].max_temp_F) - 20)
-    .attr("fill", "black")
-    
-    innerChart
+    .attr("fill", "black");
+
+  innerChart
     .append("line")
     .attr("x1", xScale(data[data.length - 4].date))
     .attr("y1", yScale(data[data.length - 4].max_temp_F) - 2)
@@ -230,9 +238,9 @@ const drawLineChart = (data) => {
     .attr("y2", +innerChart.select(".label-max-temp").attr("y") - 2)
     .attr("stroke", "black")
     .attr("stroke-width", 1);
-    
-    /*   label - average temperature   */
-    innerChart
+
+  /*   label - average temperature   */
+  innerChart
     .append("text")
     .text("Average temperature")
     .attr("class", "label-avg-temp")
@@ -240,9 +248,9 @@ const drawLineChart = (data) => {
     .attr("y", yScale(data[data.length - 1].avg_temp_F))
     .attr("fill", "black")
     .attr("dominant-baseline", "middle");
-    
-    /*   label - lowest temperature w/ line  */
-    innerChart
+
+  /*   label - lowest temperature w/ line  */
+  innerChart
     .append("text")
     .text("Lowest temperature")
     .attr("class", "label-min-temp")
@@ -294,5 +302,84 @@ const drawLineChart = (data) => {
   // Add label to the y-axis
   svg.append("text").text("Temperature (°F)").attr("y", 20);
 
-  showElapsedTime("Plotting");
+  showElapsedTime("Plotting line chart");
+};
+
+// --------------------------------------
+// Plot the arc chart with the given data
+// --------------------------------------
+const drawArcChart = (data) => {
+  showElapsedTime("Plotting arc init");
+  showElapsedTime("Plotting arc");
+
+  const pieChartWidth = 500;
+  const pieChartHeight = pieChartWidth;
+  const arcChart = d3
+    .select("#arc")
+    .append("svg")
+    .attr("viewBox", `0, 0, ${pieChartWidth}, ${pieChartHeight}`);
+  // svg객체의 크기를 viewBox로 정의한다.
+  // 반응형 웹에서 해당 요소는 viewBox에 정의한 가로 세로 비율을 유지한다.
+
+  arcChart
+    .append("rect")
+    .attr("width", pieChartWidth)
+    .attr("height", pieChartHeight)
+    .attr("fill", paperBG);
+  // .style("opacity", 0.01);
+  const innerChart = arcChart
+    .append("g")
+    .attr(
+      "transform",
+      `translate (${pieChartWidth / 2}, ${pieChartHeight / 2})`,
+    );
+
+  const numberOfDays = data.length;
+  const numberOfDaysWithPrecip = data.filter(
+    (d) => d.total_precip_in > 0,
+  ).length;
+  const percentageDaysWithPrecip = Math.round(
+    (numberOfDaysWithPrecip / numberOfDays) * 100,
+  );
+
+  console.log("Precip percentage", percentageDaysWithPrecip);
+
+  const angleInDeg = (360 * percentageDaysWithPrecip) / 100;
+  const angleInRad = (angleInDeg * Math.PI) / 180;
+
+  console.log("Precip percentage in RAD", angleInRad);
+
+  // 원호 생성기를 선언해 원호를 그릴 객체 함수를 준비한다.
+  // 객체 함수에 접근자 함수를 추가하면 원호의 모양을 지정할 수 있다.
+  // 아래 네 속성은 정적 바인딩으로 선언한 상태이다. 
+  // 해당 속성 모두를 path 요소 추가 시점에 동적으로 바인딩하는 것 또한 가능하다.
+  // 이 예시처럼 혼합형으로 바인딩하는 것도 가능하다.
+  const arcGenerator = d3
+    .arc() // 생성자 함수를 생성한다.
+    .innerRadius(90) // 호의 안지름을 지정한다
+    .outerRadius(180) // 호의 바깥지름을 지정한다
+    .padAngle(0) // 중심축 한 개에 여러 개의 호가 이어질 때 호 사이의 간격을 정한다.
+    // .cornerRadius(0); // 각 호의 시작과 끝에 있는 모서리에 내접하는 원의 반지름을 정한다.
+    // 위 값이 커지면 모서리가 완만하고 작아지면 날카롭게 된다.
+
+
+  innerChart
+    .append("path") // 호를 그릴 SVG Path 요소를 추가하고
+    .attr("d", () => { // 속성 d를 추가한다.
+      return arcGenerator({ // 호 생성 함수 arcGenerator가 아래 지시에 따라 속성 d에 들어갈 값을 생성한다.
+        startAngle: 0, // 호가 시작하는 각도를
+        endAngle: angleInRad, // 호가 끝나는 각도를 지정한다.
+      }); // D3 arc()객체 형식으로 전달한다. 
+    })
+    .attr("fill", "#4d4b44");
+
+  innerChart
+    .append("path")
+    .attr("d", () => {
+      return arcGenerator({
+        startAngle: angleInRad,
+        endAngle: 2 * Math.PI
+      });
+    })
+    .attr("fill", "#b3b0a0");
 };
