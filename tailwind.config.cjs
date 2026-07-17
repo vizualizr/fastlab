f702bfa29787accd6d1aaf5e560685d414116481
makr <cto@vizualizer.com>
2026-07-17 10:10:35 +0800

    - css pipeline rearranged   - postCSS removed.   - vite is sole in charge of css handling with tailwindcss plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./om/**/*.{html,js}",
-    // "./d3/**/*.{html,js}",
-    "./node_modules/flyonui/dist/js/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import { directoryPlugin } from "vite-plugin-list-directory-contents";
 import devtoolsJson from "vite-plugin-devtools-json";
+import tailwindcss from "@tailwindcss/vite"; 
 
 // ES 모듈에서 __dirname 대신 사용하는 표준 방식
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   css: {
-    postcss: "./postcss.config.cjs",
+    // 👈 2. 기존 postcss 지정을 제거했습니다. Vite가 Tailwind를 직접 처리하므로 필요 없습니다.
     devSourcemap: true,
   },
   server: {
@@ -19,6 +20,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    tailwindcss(), 
     devtoolsJson(),
     directoryPlugin({
       baseDir: __dirname,
@@ -30,6 +32,7 @@ export default defineConfig({
     // 실시간으로 파일을 컴파일 하므로 HMR은 정상 작동한다.
     entries: [
       "index.html",
+      "**/d3-in-action",
       "**/end/index.html",
       "**/start/index.html",
       "**/started.index.html",
